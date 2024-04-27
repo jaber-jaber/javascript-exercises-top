68fd915ef38a5cac6c5e3d9ff8f544f6e78996b4
Jaber Jaber <jaberabdulhalim@gmail.com>
2024-04-27 18:54:12 +1000

    Finished ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const revArray = string.split("");;
+    let reversedString = '';
 
+    reversedString = revArray.reverse().toString()
+    
+    for (i = 0; i < reversedString.length; i++) {
+      reversedString = reversedString.replace(',','')
+
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
