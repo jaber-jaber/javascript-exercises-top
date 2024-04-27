7566671e7eb32b5f6a0498120e635eec9857b612
Jaber Jaber <jaberabdulhalim@gmail.com>
2024-04-27 18:33:07 +1000

    Finished ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    const ERROR = "ERROR";
+
+    if (num < 0) return ERROR;
+    
+    let repeatString = '';
+
+    for (let i = 0; i < num; i++) {
+        repeatString += string;
+    }
+
+    return repeatString;
 };
 
 // Do not edit below this line
