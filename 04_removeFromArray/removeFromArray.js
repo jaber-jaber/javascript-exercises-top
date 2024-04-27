d706b63b585ccec5caec1f12ecddb5ad04d64bb6
Jaber Jaber <jaberabdulhalim@gmail.com>
2024-04-27 19:37:12 +1000

    Finished ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,28 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, valRemoved, ...multiVal) {
+  
+    for (i=0;i<array.length;i++) {
+      indexOfVal = array.indexOf(valRemoved)
+      
+      if (indexOfVal < 0) {
+        continue;
+      } else {
+        array.splice(indexOfVal, 1)
+      }
+  }
+    
+    for (i=0;i<multiVal.length;i++) {
+      indexOfVal = array.indexOf(multiVal[i]);
+      if (indexOfVal < 0) {
+        continue;
+      } else {
+      array.splice(indexOfVal, 1)
+      }
+    }
+    
+    return array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
