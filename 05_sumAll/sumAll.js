e1e0eb08aae7aaec9e11c15e86d906321d847a53
Jaber Jaber <jaberabdulhalim@gmail.com>
2024-04-28 19:04:46 +1000

    Finished ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
+    const ERROR = "ERROR";
 
+    if (typeof(x) == typeof(y)) {
+        if (x < 0 || y < 0) {
+            return ERROR;
+        } else if (x > y) {
+            for (let num = y; num <= x; num++) {
+                sum = sum + num;
+            }
+        } else if (x <= y) {
+            for (let num = x; num <= y; num++) {
+                sum = sum + num;
+            }
+        }
+    } else {
+        return ERROR;
+    }
+    
+
+    return sum;
 };
 
 // Do not edit below this line
