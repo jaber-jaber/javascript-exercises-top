c7b7fe10d87e430f21d2e9cde84755c89204021c
Jaber Jaber <jaberabdulhalim@gmail.com>
2024-05-01 19:39:44 +1000

    Finished ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let prev = 0;
+    let next = 1;
+    let fNumber;
+    let intIndex = parseInt(index);
 
+    if (intIndex === 1) {
+        return 1;
+    }
+    
+    if (intIndex === 0) {
+        return 0;
+    } else if (intIndex < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 2; i <= intIndex; i++) {
+            fNumber = prev + next;
+            prev = next;
+            next = fNumber;
+        }
+    }
+    
+    return fNumber;
 };
 
 // Do not edit below this line
